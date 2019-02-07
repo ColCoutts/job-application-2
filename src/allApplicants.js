b536d1d6ead66eef2cad7879f8cf929d03ab1f58
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-02-07 12:32:11 -0800

    this morning code cooking etc.

--- src/allApplicants.js
+++ src/allApplicants.js
@@ -14,18 +14,26 @@ for(let i = 0; i < applicant.length; i++){
     const applicantSelect = applicant[i];
     const tr = document.createElement('tr');
 
+    //has taken out name in all applicants section
     const nameCell = document.createElement('td');
-    nameCell.textContent = applicantSelect.name;
+    const a = document.createElement('a');
+    a.href = 'applied.html?name=' + encodeURIComponent(applicantSelect.name);
+    a.textContent = applicantSelect.name;
+    nameCell.appendChild(a);
     tr.appendChild(nameCell);
-
+    
+    // const nameCell = document.createElement('td');
+    // nameCell.textContent = applicantSelect.name;
+    // tr.appendChild(nameCell);
+    
     const cityCell = document.createElement('td');
     cityCell.textContent = applicantSelect.city;
     tr.appendChild(cityCell);
-
+    
     const stateCell = document.createElement('td');
     stateCell.textContent = applicantSelect.state;
     tr.appendChild(stateCell);
-
+    
     const prankCell = document.createElement('td');
     prankCell.textContent = applicantSelect.prank;
     tr.appendChild(prankCell);
@@ -37,6 +45,15 @@ for(let i = 0; i < applicant.length; i++){
     const badnessCell = document.createElement('td');
     badnessCell.textContent = applicantSelect.slider;
     tr.appendChild(badnessCell);
-
+    
     tbody.appendChild(tr);
 }
+
+
+
+// const nameCell = document.createElement('td');
+// const a = document.createElement('a');
+// a.href = 'all-applicants.html?name=' + encodeURIComponent(applicant.name);
+// a.textContent = applicant.name;
+// nameCell.appendChild(a);
+// tr.appendChild(nameCell);
--- src/applicant-applied.js
+++ src/applicant-applied.js
@@ -1,13 +1,27 @@
 const json = window.localStorage.getItem('applicants');
 // double check that this isn't applicant
+let allApplicants = [];
+
 let applicant = null;
 
 if(json) {
-    const allApplicants = JSON.parse(json);
-    applicant = allApplicants[allApplicants.length - 1];
+    allApplicants = JSON.parse(json);
 } else {
     window.location = '/';
 }
+
+const searchParams = new URLSearchParams(window.location.search);
+const nameToFind = searchParams.get('name');
+console.log(nameToFind);
+
+for(let i = 0; i < allApplicants.length; i++) {
+    const currentApplicant = allApplicants[i];
+
+    if(currentApplicant.name === nameToFind) {
+        applicant = currentApplicant;
+    }
+} 
+
 
 
 const name = document.getElementById('name');
@@ -26,3 +40,24 @@ state.textContent = applicant.state;
 pranks.textContent = applicant.prank;
 weapon.textContent = applicant.weapon.join(', ');
 slider.textContent = applicant.slider;
+
+
+
+// if(json) {
+//     const allApplicants = JSON.parse(json);
+//     // applicant = allApplicants[allApplicants.length - 1];//will not need
+// } else {
+//     window.location = '/';
+// }
+
+// const searchParam = new URLSearchParams(window.location.search);
+// const nameToFind = searchParam.get('name');
+
+// for(let i = 0; i < allApplicants.length; i++) {
+//     const currentApplicant = allApplicants[i];
+
+//     if(currentApplicant === nameToFind) {
+//         applicant = currentApplicant;
+//         break;
+//     }
+// }
